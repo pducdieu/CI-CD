02496cf1f694fa0ada0fac44e08f4a6bef1ddb5e
pducdieu <pducdieu17102002@gmail.com>
2024-04-07 19:06:20 +0700

    Divide and get the remainder

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Write Number
+        Console.Write("Nhập số thứ nhất: ");
+        int Number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nhập số thứ hai: ");
+        int Number2 = Convert.ToInt32(Console.ReadLine());
+
+        // Sum
+        int Sum = Number1 + Number2;
+
+        // divide and get the remainder
+        int Remainder = Sum % 10;
+
+        // In kết quả ra màn hình
+        Console.WriteLine("Tổng của hai số là: " + Sum);
+        Console.WriteLine("Dư khi chia tổng cho 10 là: " + Remainder);
+
+        Console.ReadLine(); // Để chương trình không đóng ngay sau khi in kết quả
+    }
+}
